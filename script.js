2bff29aa2c8a3cf0d33ad100733dabcb53933401
Gabriel <130710570+gabisantosdev@users.noreply.github.com>
2024-08-16 19:06:12 -0300

    fix: Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
 function mostrarMensagem() {
     document.getElementById("mensagem").innerText = "Portfólio em desenvolvimento"
     document.getElementById("gitHubButton").style.display = "block"
-}
+} 
